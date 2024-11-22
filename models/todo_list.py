08f7ecf4039118dbaa783d0df7392d773fd15f8a
zunedisinambela <zunedisinambela@gmail.com>
2024-11-22 16:38:29 +0700

    staterpack

---
+++ models/todo_list.py
@@ -0,0 +1,10 @@
+from odoo import api, fields, models
+
+
+class OwlTodo(models.Model):
+    _name = 'owl.todo.list'
+    _description = 'OWL Todo List App'
+
+    name = fields.Char(string='Task Name')
+    completed = fields.Boolean()
+    color = fields.Char()
